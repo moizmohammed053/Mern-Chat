7bd6b49c7eb1facb7b11f4792bdf0d79e3d2f28f
Mohd.Moiz Ahmed <mohd.moiz@outlook.com>
2025-01-06 17:32:09 -0500

    first commit

--- Client/src/Register.jsx
+++ Client/src/Register.jsx
@@ -6,7 +6,7 @@ export default function Register (){
     return (
         <div className="bg-blue-50 h-screen flex items-center"> 
         <form className="w-64 mx-auto mb-12">
-            <input value={username} Change={ev => setUsername(ev.target.value)} type="text" placeholder="username" className="block w-full rounded-sm p-2 mb-2 border" />
+            <input value={username} onChange={ev => setUsername(ev.target.value)} type="text" placeholder="username" className="block w-full rounded-sm p-2 mb-2 border" />
             <input value={password} onChange={ev => setPassword(ev.target.value)} type="password" placeholder="password" className="block w-full rounded-sm p-2 mb-2 border"/>
             <button className="bg-blue-500 text-white block w-full rounded-sm p-2 ">
                 RETISTER Now
