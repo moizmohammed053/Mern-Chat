b6df1327acad63eaae4fd0d3d1d1794e32b69816
Mohd.Moiz Ahmed <mohd.moiz@outlook.com>
2025-01-07 20:56:06 -0500

    first commit

--- Client/src/Register.jsx
+++ Client/src/Register.jsx
@@ -1,7 +1,7 @@
 import {useState} from "react"
 export default function Register (){
     const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    [password, setPassword] = useState('');
 
     return (
         <div className="bg-blue-50 h-screen flex items-center"> 
