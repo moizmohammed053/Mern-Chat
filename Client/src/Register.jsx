5c249c223309467d86ab7b034acf8602724a2587
Mohd.Moiz Ahmed <mohd.moiz@outlook.com>
2025-01-09 08:00:55 -0500

    first commit

--- Client/src/Register.jsx
+++ Client/src/Register.jsx
@@ -3,7 +3,7 @@ export default function Register (){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    return (
+    retur (
         <div className="bg-blue-50 h-screen flex items-center"> 
         <form className="w-64 mx-auto mb-12">
             <input value={username} onChange={ev => setUsername(ev.target.value)} type="text" placeholder="username" className="block w-full rounded-sm p-2 mb-2 border" />
